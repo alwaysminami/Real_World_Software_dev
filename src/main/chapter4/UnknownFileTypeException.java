ae765c5d9d3c00d25f8faa4e8c92e0c2941c1839
alwaysminami <kimyesung2000@gmail.com>
2023-07-28 17:30:07 +0900

    230728 chap4 end

---
+++ src/main/chapter4/UnknownFileTypeException.java
@@ -0,0 +1,7 @@
+package main.chapter4;
+
+public class UnknownFileTypeException extends RuntimeException {
+    public UnknownFileTypeException(final String message) {
+        super(message);
+    }
+}
